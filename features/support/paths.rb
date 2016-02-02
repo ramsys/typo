40d1fd857b46bc160313c45a29a3b2793ed51145
ramsys <alblaita@gmail.com>
2016-02-03 00:06:34 +0100

    First scenario merge feature

--- features/support/paths.rb
+++ features/support/paths.rb
@@ -20,6 +20,10 @@ module NavigationHelpers
     when /^the new category page$/
       '/admin/categories/new'
 
+    when /^the article page for "(.*)"$/i
+      "/admin/content/edit/#{Article.find_by_title($1).id}"
+
+
     # Add more mappings here.
     # Here is an example that pulls values out of the Regexp:
     #
